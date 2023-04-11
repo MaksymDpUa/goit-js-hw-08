18364b0fb1040fe140a72b427cb7c002ad5436f5
Maksym Ponomarenko <ponomarenko2983@gmail.com>
2023-04-11 14:38:26 +0200

    finished task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const STORAGE_KEY = 'videoplayer-current-time';
+
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(data.seconds));
+  }, 1000)
+);
+
+player.setCurrentTime(JSON.parse(localStorage.getItem(STORAGE_KEY))).then(function(seconds) {
+ 
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
