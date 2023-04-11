d117db3a1940ba79700ef6b0c18d67f0c04d13ff
Maksym Ponomarenko <ponomarenko2983@gmail.com>
2023-04-11 13:05:09 +0200

    finished task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 console.log(galleryItems);
+const galleryMarkup = makeGalerry(galleryItems).join("");
+const gallery = document.querySelector(".gallery");
+
+function makeGalerry(arr) {
+  return arr.map(
+    (item) => `<li class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" alt="${item.description}"/> </a></li>`
+  );
+}
+
+gallery.insertAdjacentHTML("beforeend", galleryMarkup);
+
+gallery.addEventListener("click", onClick);
+
+function onClick(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+ 
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+  lightbox.open();
+}
